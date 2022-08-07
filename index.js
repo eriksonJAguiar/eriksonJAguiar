88508646b051174b68bae247cfd6581f95747165
Erikson Aguiar <erjulioaguiar@gmail.com>
2022-08-07 17:06:09 -0300

    remove portifolio

--- index.js
+++
@@ -1,8 +0,0 @@
-const animateBtn = document.querySelector('#navbar__icon');
-const animateActive = document.querySelector('.active');
-const body = document.querySelector('body');
-
-animateBtn.onclick = () => {
-    animateBtn.classList.toggle('fa-times');
-    body.classList.toggle('noscroll');
-};
